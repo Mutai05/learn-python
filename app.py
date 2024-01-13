1c9d4f60ca53d1a833f9b6fd8a47cbe95509d88e
Mutai05 <kelvok2014@gmail.com>
2024-01-13 23:41:54 +0300

    variables and user input

---
+++ app.py
@@ -0,0 +1,17 @@
+# variables
+
+age = 20
+price = 19.95
+first_name = "Kelvin"
+is_Online = True
+
+print(age)
+print(price)
+print(first_name)
+print(is_Online)
+
+# receiving input from the user
+
+name = input("What is your name? ")
+print("Hello " + name)
+
