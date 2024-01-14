c74c9ffb85f0be81f3e9a3b50fe3c48d8908447b
Mutai05 <kelvok2014@gmail.com>
2024-01-14 16:06:30 +0300

    for loop

---
+++ forloops.py
@@ -0,0 +1,13 @@
+# for loop
+
+numbers = [1, 2, 3, 4, 5, 6, 7, 8]
+
+for item in numbers:
+    print(item)
+
+# implement while loop
+
+i = 0
+while i < len(numbers):
+    print(numbers[i])
+    i = i + 1
