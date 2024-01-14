70bd4e9bf02d10b0ae6ef5eea0a02150406bebb5
Mutai05 <kelvok2014@gmail.com>
2024-01-14 13:43:46 +0300

    more on if else statements

--- ifstatements.py
+++ ifstatements.py
@@ -1,10 +1,13 @@
 # if statements
 
-temp = 25
+temp = 5
 
 if temp > 30:
     print("It is a hot day")
     print("Drink plenty of water")
 elif temp > 20:
     print("It is a nice day")
-print("Done")
+elif temp < 10:
+    print("It is a cold day")
+    print("Put on sweaters")
+print("Done!")
