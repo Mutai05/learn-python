20d519b2669a5c5d9b46e47936b2949ed288be7b
Mutai05 <kelvok2014@gmail.com>
2024-01-14 15:55:13 +0300

    more on lists

--- lists.py
+++ lists.py
@@ -6,3 +6,12 @@ print(names)
 print(names[-2])
 print(names[-1])
 print(names[0:3])
+
+# append method
+
+numbers = [1, 2, 3, 4, 5, 6, 7, 8]
+numbers.append(9)
+print(numbers)
+print(1 in numbers)
+print(len(numbers))
+
